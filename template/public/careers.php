eb56f446d9c52f1ce8934fc954c75f6fd19786bb
mscott18 <50372629+mscott18@users.noreply.github.com>
2019-11-11 10:59:47 -0600

    Updated careers and community pages

--- template/public/careers.php
+++ template/public/careers.php
@@ -5,7 +5,36 @@
 
 
 
+      <div class="careers-info">
+        <div class="jumbotron jumbotron careers-bg-cover">
+          <div class="row">
 
+          </div>
+
+        </div>
+        <h1 class="text-center">Careers<br><br></h1>
+        <h3>Voted one of the best places to work in Minnesota</h3>
+        <div class="h-divider">
+        </div>
+	       <div class="row">
+          <div class="col-6">
+            <h4><strong>Find Your career with us</strong></h4>
+            <h5>We’re a coordinated health care organization that includes primary care services and a clinic.
+				        We offer jobs and careers for physicians, nurses and other health care professionals. Administrative
+        				positions include those in business and billing operations, technology, and customer service.
+				        No matter what type of health care career you are looking for, we have one thats right for you.</h5>
+          </div>
+          <div class="col-6">
+            <h4><strong>A great place to work</strong></h4>
+            <h5>We have a great culture, with amazing benefits. Voted one of the top places to work in Minnesota,
+				        you will love being a part of our team. Metro Med takes pride in our diversity and sustainability
+				        as we go above and beyond to make our community as healthy as it can be. We understand that our staff
+				        is what makes our medical center what it is adn we take care of our own.</h5>
+          </div>
+
+        </div>
+
+      </div>
 
 
 
