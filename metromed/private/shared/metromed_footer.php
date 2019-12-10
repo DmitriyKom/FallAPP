3337c2afe1f6b3c8432a7bd650ad9c8e734b8f9f
mcneelyz <45441734+mcneelyz@users.noreply.github.com>
2019-12-09 18:15:43 -0600

    fixed footer links

--- metromed/private/shared/metromed_footer.php
+++ metromed/private/shared/metromed_footer.php
@@ -1,7 +1,7 @@
     </div>
 
     <footer class="page-footer">
-      <p>&copy; <?php echo Date('Y'); ?>, Metro Medical Center |<a href="privacy.php"> Privacy Policy </a>| <a href="terms.php">Terms of Service</p></a>
+      <p>&copy; <?php echo Date('Y'); ?>, Metro Medical Center |<a href="<?php echo url_for('privacy.php') ?>"> Privacy Policy </a>| <a href="<?php echo url_for('terms.php') ?>">Terms of Service</p></a>
       <!-- Template built for Bootstrap by @mdo -->
       <!-- Images Designed by Freepik -->
     </footer>
